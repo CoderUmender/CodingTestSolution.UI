2b685531e6a504c3f33d270266437ac940f2ce7a
Abbati, Umender <UAbbati@geico.com>
2019-12-09 05:47:29 -0500

    Added Edit

--- src/app/location/edit.component.ts
+++ src/app/location/edit.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, NgForm } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { LocationService } from '../Services/location.service';
 
+import { location } from 'src/app/models/location';
 @Component({
   selector: 'app-edit',
   templateUrl: './edit.component.html',
@@ -7,9 +11,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditComponent implements OnInit {
 
-  constructor() { }
+  locationform: FormGroup;
+  location: location;
+  loading = false;
+  submitted = false;
+  locationid: string;
+  constructor(private formBuilder: FormBuilder,
+              private route: ActivatedRoute,
+              private router: Router,
+              private locationservice: LocationService
+    ) { }
 
   ngOnInit() {
-  }
+    this.locationid = this.route.snapshot.paramMap.get('Location_Id');
+    this.locationform = this.formBuilder.group({
+      Description: ['', Validators.required]
+  });
+}
+get f() { return this.locationform.controls; }
+onSubmit() {
+
+    this.location.Description =  this.locationform.get('Description').value;
+    this.location.Location_Id = Number(this.locationid);
+    this.locationservice.edit(this.location);
+      }
 
 }
